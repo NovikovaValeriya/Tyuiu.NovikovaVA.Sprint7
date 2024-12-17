66e3e8a63e376422038dbb284af13d0bd8a7e1cf
NovikovaValeriya <valeriya_novikova_06@mail.ru>
2024-12-17 20:59:36 +0500

    Add project files.

---
+++ Tyuiu.NovikovaVA.Sprint7.Project.V2.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.NovikovaVA.Sprint7.Project.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.NovikovaVA.Sprint7.Project.V2/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.NovikovaVA.Sprint7.Project.V2
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
